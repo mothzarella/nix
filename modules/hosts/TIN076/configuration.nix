7eef45371790baf3ad5b05a82a3cb7bf85598262
tar <git@mothzarella.dev>
2026-08-31 18:43:27 +0200

    feat: add TIN076 host

---
+++ modules/hosts/TIN076/configuration.nix
@@ -0,0 +1,60 @@
+{
+  config,
+  secret,
+  ...
+}: let
+  inherit (config.flake.modules) nixos;
+in {
+  flake.modules.nixos.TIN076 = {lib, ...}: {
+    imports = with nixos; [
+      aeromoe
+      btrfs-rollback
+      performance
+      preservation
+      secure-boot
+      tar
+    ];
+
+    hardware.facter = {
+      # reportPath = ./facter.json;
+      detected.boot.graphics.kernelModules = lib.mkForce ["i915"];
+    };
+
+    services.displayManager = {
+      sddm = {
+        enable = true;
+        wayland.enable = true;
+      };
+      autoLogin = {
+        enable = true;
+        user = "tar";
+      };
+    };
+
+    # ------------------------------------------------------------- preservation
+    preservation.preserveAt."/persistent" = {
+      directories = [
+        "/var/lib/AccountsService"
+        "/var/lib/power-profiles-daemon"
+        "/var/lib/systemd/backlight"
+        "/var/lib/systemd/rfkill"
+        "/var/lib/upower"
+        (secret "/var/lib/bluetooth")
+        (secret "/var/lib/iwd")
+      ];
+
+      users.tar.directories = [
+        "Desktop"
+        "Documents"
+        # "Downloads"
+        "Music"
+        "Pictures"
+        "Public"
+        "Templates"
+        "Videos"
+      ];
+    };
+
+    system.stateVersion = "26.11";
+  };
+}
---
+++ modules/hosts/TIN076/disko.nix
@@ -0,0 +1,62 @@
+{inputs, ...}: let
+  mountOptions = ["compress=zstd" "noatime"];
+
+  layout.disko.devices.disk.main = {
+    device = "/dev/disk/by-id/nvme-TIN076";
+    type = "disk";
+
+    content.type = "gpt";
+
+    content.partitions.esp = {
+      name = "ESP";
+      type = "EF00";
+      size = "1G";
+
+      content.type = "filesystem";
+      content.format = "vfat";
+      content.mountpoint = "/boot";
+      content.mountOptions = ["umask=0077"];
+    };
+
+    content.partitions.swap = {
+      size = "4G";
+      content.type = "swap";
+    };
+
+    content.partitions.root = {
+      name = "root";
+      size = "100%";
+
+      content.type = "luks";
+      content.name = "cryptroot";
+      content.settings.allowDiscards = true;
+
+      content.content.type = "btrfs";
+      content.content.extraArgs = ["-f"];
+
+      content.content.subvolumes."/root" = {
+        inherit mountOptions;
+        mountpoint = "/";
+      };
+      content.content.subvolumes."/persistent" = {
+        inherit mountOptions;
+        mountpoint = "/persistent";
+      };
+      content.content.subvolumes."/nix" = {
+        inherit mountOptions;
+        mountpoint = "/nix";
+      };
+    };
+  };
+in {
+  imports = [inputs.disko.flakeModules.disko];
+
+  flake = {
+    diskoConfigurations.TIN076 = layout;
+
+    modules.nixos.TIN076 = {
+      imports = [inputs.disko.nixosModules.disko];
+      inherit (layout) disko;
+    };
+  };
+}
